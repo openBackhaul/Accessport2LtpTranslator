3d75c6abd4ba9292ec41548194808fd53d3d00f8
Martin Fuchs <m.fuchs@mentopolis.de>
2024-01-16 14:08:49 +0100

    implement StringProfileService

--- server/service/StringProfileService.js
+++ server/service/StringProfileService.js
@@ -1,14 +1,30 @@
 'use strict';
 
+const fileOperation = require('onf-core-model-ap/applicationPattern/databaseDriver/JSONDriver');
+const ProfileCollection = require('onf-core-model-ap/applicationPattern/onfModel/models/ProfileCollection');
+const Profile = require('onf-core-model-ap/applicationPattern/onfModel/models/Profile');
+const onfAttributes = require('onf-core-model-ap/applicationPattern/onfModel/constants/OnfAttributes');
+const createHttpError = require('http-errors');
+
+const profileConstants = Object.freeze({
+  OPERATION_MODE_REACTIVE: 'string-profile-1-0:STRING_VALUE_TYPE_REACTIVE',
+  OPERATION_MODE_PROTECTION: 'string-profile-1-0:STRING_VALUE_TYPE_PROTECTION',
+  OPERATION_MODE_OFF: 'string-profile-1-0:STRING_VALUE_TYPE_OFF',
+});
 
 /**
  * Returns the enumeration values of the String
  *
- * uuid String 
+ * uuid String
  **/
-exports.getStringProfileEnumeration = function(uuid) {
+exports.getStringProfileEnumeration = async function (url) {
+  let value = await fileOperation.readFromDatabaseAsync(url);
+  if (!value) {
+    value = [];
+  }
+
   return {
-    "string-profile-1-0:enumeration" : [ "string-profile-1-0:STRING_VALUE_TYPE_REACTIVE", "string-profile-1-0:STRING_VALUE_TYPE_PROTECTION", "string-profile-1-0:STRING_VALUE_TYPE_OFF" ]
+    "string-profile-1-0:enumeration": value
   };
 }
 
@@ -16,35 +32,40 @@ exports.getStringProfileEnumeration = function(uuid) {
 /**
  * Returns the pattern of the String
  *
- * uuid String 
+ * uuid String
  **/
-exports.getStringProfilePattern = function(uuid) {
+exports.getStringProfilePattern = async function (url) {
+  let value = await fileOperation.readFromDatabaseAsync(url);
+  if (!value) {
+    value = "";
+  }
+
   return {
-    "string-profile-1-0:pattern" : "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    "string-profile-1-0:pattern": value
   };
 }
 
 
 /**
  * Returns the name of the String
- *
- * uuid String 
  **/
-exports.getStringProfileStringName = function(uuid) {
+exports.getStringProfileStringName = async function (url) {
+  const value = await fileOperation.readFromDatabaseAsync(url);
+
   return {
-    "string-profile-1-0:string-name" : "operationMode"
+    "string-profile-1-0:string-name": value
   };
 }
 
 
 /**
  * Returns the configured value of the String
- *
- * uuid String 
  **/
-exports.getStringProfileStringValue = function(uuid) {
+exports.getStringProfileStringValue = async function (url) {
+  const value = await fileOperation.readFromDatabaseAsync(url);
+
   return {
-    "string-profile-1-0:string-value" : "string-profile-1-0:STRING_VALUE_TYPE_OFF"//TODO
+    "string-profile-1-0:string-value": value
   };
 }
 
@@ -52,13 +73,43 @@ exports.getStringProfileStringValue = function(uuid) {
 /**
  * Configures value of the String
  *
- * body Stringprofileconfiguration_stringvalue_body 
- * uuid String 
+ * body Stringprofileconfiguration_stringvalue_body
  * no response value expected for this operation
  **/
-exports.putStringProfileStringValue = function(body,uuid) {
-  //TODO
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
+exports.putStringProfileStringValue = async function (body, url) {
+  const currentOperationModeValue = await exports.getOperationModeProfileStringValue();
+  const newOperationModeValue = body['string-profile-1-0:string-value'];
+
+  let profiles = await ProfileCollection.getProfileListForProfileNameAsync(Profile.profileNameEnum.STRING_PROFILE);
+  let pac = profiles[0][onfAttributes.STRING_PROFILE.PAC];
+  let capability = pac[onfAttributes.STRING_PROFILE.CAPABILITY];
+  let enumeration = capability[onfAttributes.STRING_PROFILE.ENUMERATION]
+
+  if (enumeration.includes(newOperationModeValue)) {
+    await fileOperation.writeToDatabaseAsync(url, body, false);
+    console.log(`Profile "operationMode" changed from "${currentOperationModeValue}" to "${newOperationModeValue}"`);
+  } else {
+    throw new createHttpError.BadRequest("Value of operationMode is should be Reactive, OFF or Protection.")
+  }
+}
+
+/**
+ * Returns the configured value of the "operationMode" profile
+ * @returns {Promise<String>}
+ */
+exports.getOperationModeProfileStringValue = async function () {
+  let profiles = await ProfileCollection.getProfileListForProfileNameAsync(Profile.profileNameEnum.STRING_PROFILE);
+
+  for (let profile of profiles) {
+    let pac = profile[onfAttributes.STRING_PROFILE.PAC];
+    let capability = pac[onfAttributes.STRING_PROFILE.CAPABILITY];
+
+
+    if ("operationMode" === capability[onfAttributes.STRING_PROFILE.STRING_NAME]) {
+      let configuration = pac[onfAttributes.STRING_PROFILE.CONFIGURATION];
+      return configuration[onfAttributes.STRING_PROFILE.STRING_VALUE];
+    }
+  }
+
+  throw new createHttpError.InternalServerError("OperationMode String profile not found.");
 }
